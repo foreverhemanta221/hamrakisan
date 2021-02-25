238985753f7ca33aeaae7af6b9e840ae94f2ac95
PremRaj <bindas.prem.75@gmail.com>
2021-02-25 20:23:00 +0545

    language issue update

--- app/Http/Controllers/Frontend/LanguageSwitcherController.php
+++ app/Http/Controllers/Frontend/LanguageSwitcherController.php
@@ -12,12 +12,12 @@ class LanguageSwitcherController extends Controller
 {
     public function getlang($locale=null , Request $request){
         if($locale==null || $locale=='en'){
-            $request->session()->flush();
+            // $request->session()->flush();
             App::setLocale('en');
             Session::put('locale', 'en');
 //            $request->session()->put('locale','en');
         }else{
-            $request->session()->flush();
+            // $request->session()->flush();
             App::setLocale('nep');
             Session::put('locale', 'nep');
 //            $request->session()->put('locale','nep');
